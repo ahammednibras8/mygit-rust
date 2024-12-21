3a7c9c1ebd10ead0c820d76924b48a3f52e6ac79
ahammednibras8 <ahammednibras@proton.me>
2024-12-21 21:13:29 +0530

    feat: Add `init <path>` command to initialize mygit repository

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use commands::init;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
+    let args = &args[1..];
+
     match args.get(1).map(String::as_str) {
         Some("init") => {
-            if let Err(err) = init::init() {
-                eprintln!("Error: {}", err);
+            let target_dir = args.get(2).map_or(".", String::as_str);
+            if let Err(err) = init::init(target_dir) {
+                eprintln!("{}", err);
             }
         }
-        _ => eprintln!("Usage: mygit <command>"),
+        _ => {
+            eprintln!("Usage: mygit <command>");
+            eprintln!("Commands:");
+            eprintln!("  init [path]   Initialize a new mygit repository in the specified path");
+        }
     }
 }
