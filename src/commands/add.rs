0f3e4ad82329ddbb09096e614054bc4d957efaac
ahammednibras8 <ahammednibras@proton.me>
2024-12-24 20:21:40 +0530

    Finished

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,42 +1,85 @@
+use anyhow::Result;
 use sha1::{Digest, Sha1};
-use std::fs;
-use std::path::PathBuf;
+use std::fs::{self, OpenOptions};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 pub fn add(file_path: &str) -> Result<(), anyhow::Error> {
     let current_dir = std::env::current_dir()?;
     let mygit_dir = current_dir.join(".mygit");
 
-    //Checking .mygit directory exists
+    //Check if the .mygit directory exists
     if !mygit_dir.exists() {
         return Err(anyhow::anyhow!(
             "Not a mygit repository (or any of the parent directories)"
         ));
     }
 
-    //Checking if the file exists
-    let file = PathBuf::from(file_path).canonicalize()?;
-    if !file.exists() {
-        return Err(anyhow::anyhow!("File '{}' not found", file_path));
+    //Resolve the path to the file
+    let target = PathBuf::from(file_path).canonicalize()?;
+    if !target.exists() {
+        return Err(anyhow::anyhow!("Path '{}' not found", file_path));
     }
 
-    //Read files
-    let file_contents = fs::read(file)?;
+    //Process the file
+    process_path(&target, &mygit_dir)?;
 
-    //Calculate hash using sha1
+    Ok(())
+}
+
+fn process_path(target: &Path, mygit_dir: &Path) -> Result<(), anyhow::Error> {
+    //Skip the .mygit directory
+    if target.starts_with(mygit_dir) {
+        return Ok(());
+    }
+
+    if target.is_dir() {
+        for entry in fs::read_dir(target)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+
+            process_path(&entry_path, mygit_dir)?;
+        }
+    } else if target.is_file() {
+        process_file(target.to_path_buf(), mygit_dir)?;
+    } else {
+        println!("Unsuported file type: {}", target.display());
+    }
+
+    Ok(())
+}
+
+fn process_file(file: PathBuf, mygit_dir: &Path) -> Result<(), anyhow::Error> {
+    let file_contents = fs::read(&file)?;
+
+    //calculate sha-1
     let mut hasher = Sha1::new();
     hasher.update(&file_contents);
     let hash = hasher.finalize();
     let hash_hex = format!("{:x}", hash);
 
-    //Strorng the files in .mygit/objects/<hash>
     let object_path = mygit_dir.join("objects").join(&hash_hex);
-    fs::write(object_path, file_contents)?;
 
-    //Update the index file
-    let index_entry = format!("{} {}\n", hash_hex, file_path);
-    fs::write(mygit_dir.join("index"), index_entry.as_bytes())?;
+    //Store the file contents in objects
+    if !object_path.exists() {
+        fs::write(&object_path, file_contents)?;
+        println!("Stored object: {}", object_path.display());
+    } else {
+        println!("Object already exists: {}", object_path.display());
+    }
 
-    println!("File '{}' added to staging area", file_path);
+    let file_path_relative = file.strip_prefix(std::env::current_dir()?)?;
 
+    let index_entry = format!("{} {}", hash_hex, file_path_relative.display());
+
+    //Adding entry to index
+    let index_path = mygit_dir.join("index");
+    let mut index_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&index_path)?;
+    index_file.write_all(index_entry.as_bytes())?;
+
+    println!("Added to index: {} {}", hash_hex, file_path_relative.display());
     Ok(())
 }
